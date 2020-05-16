fbb50b7036173fcd6057360a6cea0d89bdc63985
Zhang233Chen <51501067+Zhang233Chen@users.noreply.github.com>
2020-05-16 20:28:35 +0800

    Add files via upload

---
+++ ThreadPoolDemo/src/DiscardPolicyDemo.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class DiscardPolicyDemo {
+
+    private static final int THREADS_SIZE = 1;
+    private static final int CAPACITY = 1;
+
+    public static void main(String[] args) throws Exception {
+
+        // create the thread pool corePoolSize = maximumPoolSize = 1，blockingQueue capacity = 1.
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // The reject strategy is DiscardPolicy
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+
+        // new 10 tasks and add them into the thread pool.
+        for (int i = 0; i < 10; i++) {
+            Runnable myrun = new MyRunnable("task-"+i);
+            pool.execute(myrun);
+        }
+        // close thread pool
+        pool.shutdown();
+    }
+}
